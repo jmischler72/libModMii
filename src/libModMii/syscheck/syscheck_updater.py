a0c343ff792aaf6c53750dd46620f5e0453149b0
Joffrey <joffrey.mischlerps@gmail.com>
2025-09-07 18:56:51 +0200

    refactor: simplify syscheck data analysis and enhance return structure

--- src/libModMii/syscheck/syscheck_updater.py
+++ src/libModMii/syscheck/syscheck_updater.py
@@ -5,12 +5,7 @@ from . import validation_helpers as validation
 class SyscheckError(Exception):
     pass
 
-def analyse_syscheck_data(
-    data: str,
-    activeIOS: bool = False,
-    extraProtection: bool = False,
-    cMios: bool = False,
-) -> Dict[str, Any]:
+def get_syscheck_infos(data: str) -> Dict[str, Any]:
     data = validation.translate_keywords_to_english(data)
     
     if not validation.validate_syscheck_data(data):
@@ -34,20 +29,35 @@ def analyse_syscheck_data(
             f'The firmware region "{firmware["SMregion"]}" does not match the console region "{region_short_code}"'
         )
 
+    return {
+        'region': region,
+        'hbcVersion': hbc_version,
+        'systemMenuVersion': system_menu_version,
+        'firmware': {
+            'SMregion': firmware['SMregion'],
+            'firmware': firmware['firmware'],
+            'firmwareVersion': firmware['firmwareVersion'],
+        },
+        'consoleType': console_type,
+    }
+
+def get_syscheck_analysis(data: str, activeIOS: bool = False, extraProtection: bool = False) -> Dict[str, Any]:
+    infos = get_syscheck_infos(data)
+    
     wad_to_install = []
 
     # Check system components
     is_bootmii_installed = validation.check_if_bootmii_installed(data)
     is_priiloader_installed = validation.check_if_priiloader_installed(data)
-    is_hbc_outdated = validation.check_if_hbc_is_outdated(hbc_version, console_type) if hbc_version else False
-    outdated_d2xcios = validation.check_d2x_cios(data, console_type)
-    missing_ios = validation.check_for_missing_ios(data, region, console_type) if activeIOS else []
+    is_hbc_outdated = validation.check_if_hbc_is_outdated(infos.hbcVersion, infos.consoleType) if infos.hbcVersion else False
+    outdated_d2xcios = validation.check_d2x_cios(data, infos.consoleType)
+    missing_ios = validation.check_for_missing_ios(data, infos.region, infos.consoleType) if activeIOS else []
     needs_extra_protection = validation.check_extra_protection(data) if extraProtection else []
 
     if not is_bootmii_installed:
         wad_to_install.append('HM')
     else:
-        if not hbc_version:
+        if not infos.hbcVersion:
             wad_to_install.append('OHBC113')
             # also check if IOS58 is installed
             is_ios58_installed = 'IOS58' in data
@@ -57,9 +67,9 @@ def analyse_syscheck_data(
             if is_hbc_outdated:
                 wad_to_install.append('OHBC')
 
-    latest_firmware_version = info.get_latest_sm_version(firmware)
-    if latest_firmware_version != firmware['firmware']:
-        wad_to_install.append(f'SM{latest_firmware_version}{firmware["SMregion"]}')
+    latest_firmware_version = info.get_latest_sm_version(infos.firmware)
+    if latest_firmware_version != infos.firmware['firmware']:
+        wad_to_install.append(f'SM{latest_firmware_version}{infos.firmware["SMregion"]}')
 
     update_priiloader = False
     if not is_priiloader_installed or (is_priiloader_installed and update_priiloader):
@@ -76,23 +86,4 @@ def analyse_syscheck_data(
     if wad_to_install:
         wad_to_install.append('yawm')
 
-    return {
-        'region': region,
-        'hbcVersion': hbc_version,
-        'systemMenuVersion': system_menu_version,
-        'firmware': {
-            'SMregion': firmware['SMregion'],
-            'firmware': firmware['firmware'],
-            'firmwareVersion': firmware['firmwareVersion'],
-        },
-        'consoleType': console_type,
-        'systemChecks': {
-            'isBootMiiInstalled': is_bootmii_installed,
-            'isPriiloaderInstalled': is_priiloader_installed,
-            'isHbcOutdated': is_hbc_outdated,
-            'missingIOS': missing_ios,
-            'outdatedD2XCios': outdated_d2xcios,
-            'needsExtraProtection': needs_extra_protection,
-        },
-        'wadToInstall': wad_to_install,
-    }
+    return wad_to_install
